760ac2acb911c306e60e7eb37053ed9d3ace9f33
Şamil Güser <samil.guser@sestek.com>
2021-01-16 20:02:17 +0300

    Raw List Class Baed View

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from .views import (
     my_fbv,
-    CourseView
+    CourseView,
+    CourseListView,
+    MyCourseListView
 )
 
 app_name = "courses"
 urlpatterns = [
     # path('', my_fbv, name='courses-list')
 
-    path('', CourseView.as_view(template_name='contact.html'), name='article-list'),
+    path('', CourseListView.as_view(), name='article-list'),
     # path('create/', ArticleCreateView.as_view(), name='article-create'),
     path('<int:id>/', CourseView.as_view(), name='courses-detail'),
     # path('<int:id>/update', ArticleUpdateView.as_view(), name='article-update'),
--- src/courses/views.py
+++ src/courses/views.py
@@ -3,6 +3,24 @@ from django.views import View  # BASE VIEW CLASS
 from .models import Course
 
 
+class CourseListView(View):
+    template_name = "courses/course_list.html"
+    queryset = Course.objects.all()
+
+    def get_queryset(self):
+        return self.queryset
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            'object_list': self.get_queryset()
+        }
+        return render(request, self.template_name, context)
+
+
+class MyCourseListView(CourseListView):
+    queryset = Course.objects.filter(id=1)
+
+
 class CourseView(View):
     template_name = "courses/course_detail.html"
 
